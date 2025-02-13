f117522b3073a0cc21e4f9479678410d6d9c3090
sachmilan1 <144947789+sachmilan1@users.noreply.github.com>
2025-02-13 02:15:03 -0500

    pushing the checker

--- src/test/java/com/OntarioTechnicalSolutions/demo/DemoApplicationTests.java
+++ src/test/java/com/OntarioTechnicalSolutions/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.OntarioTechniacalSolutions.demo;
+package com.OntarioTechnicalSolutions.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
