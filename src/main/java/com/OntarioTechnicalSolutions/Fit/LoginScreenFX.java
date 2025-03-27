9044b3ec8b5438fb3d2615fcbbcb8e3dba6c2355
sachmilan1 <144947789+sachmilan1@users.noreply.github.com>
2025-03-27 12:44:20 -0400

    uploading the uopdated pom.xml file to compile a jar file

--- src/main/java/com/OntarioTechnicalSolutions/Fit/LoginScreenFX.java
+++ src/main/java/com/OntarioTechnicalSolutions/Fit/LoginScreenFX.java
@@ -1,12 +1,9 @@
 package com.OntarioTechnicalSolutions.Fit;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
@@ -15,12 +12,11 @@ public class LoginScreenFX extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("FXML/LoginScreen.fxml"));
-        Parent root = fxmlLoader.load();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/LoginScreen.fxml"));
+        Parent root = loader.load();
         stage.getIcons().add(new Image("images/icon.png"));
         stage.setTitle("Fitness App");
         stage.setScene(new Scene(root));
         stage.show();
     }
-
 }
